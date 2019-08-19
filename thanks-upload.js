1d049a7123ecce5232e5044c18d31664bf91878a
Mozart <mozart.iago@gmail.com>
2019-08-19 13:24:25 -0300

    Create thanks-upload.js

---
+++ thanks-upload.js
@@ -0,0 +1,7 @@
+// ==thanks-upload==
+// name   Thanks Upload FW
+// ==/thanks-upload==
+"use strict";
+if ($("div#thanks_button_container").length !== 0) { 
+  postador_thank_upload(document.getElementById('qr_threadid').value) 
+}
